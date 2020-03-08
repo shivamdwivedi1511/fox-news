9ef1f0d2a1e9d1402ece3f643025dcdf1c84d449
shivamdwivedi-kiwi <shivamdwivedi1511@gmail.com>
2020-03-08 22:08:45 +0530

    added the firebase config for deployment

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,10 +1,10 @@
 export const API_URL={
     ALL_SOURCES:{
-        url:`http://newsapi.org/v2/sources?apiKey=${process.env.REACT_APP_NEWS_API}`,
+        url:`https://newsapi.org/v2/sources?apiKey=${process.env.REACT_APP_NEWS_API}`,
         type:'GET'
     },
     SOURCE_NEWS_LIST:{
-        url:`http://newsapi.org/v2/everything?apiKey=${process.env.REACT_APP_NEWS_API}`,
+        url:`https://newsapi.org/v2/everything?apiKey=${process.env.REACT_APP_NEWS_API}`,
         type:'GET'
     }
 }
